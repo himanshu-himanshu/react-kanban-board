9ee98a7908532735c76767306930c4d03d9e9794
Himanshu <himanshu16315@gmail.com>
2023-02-24 16:10:08 -0500

    Restructured project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex justify-center items-center p-4">
+      <h1 className="text-6xl text-pink-600">React Kanban</h1>
+    </div>
+  );
+};
+
+export default Header;
